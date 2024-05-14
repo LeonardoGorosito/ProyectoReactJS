b0259ebc6e31b277be95264528cc018d76986566
LeoG <leogorosito.lg@gmail.com>
2024-05-13 22:30:36 -0300

    Agregando primer components

---
+++ src/components/Menu/menu.jsx
@@ -0,0 +1,16 @@
+const Menu = () => {
+    return (
+        <div className="navbar">
+            <h1 className="logo">eCommercce</h1>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Acerca de nosotros</li>
+            <li>Sppnsors</li>
+            <li>Carrito</li>
+        </div>
+
+    )
+    
+  }
+
+  export default Menu
